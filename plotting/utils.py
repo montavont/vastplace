ede3f288804df9ea74433f51f75ad912289acb8a
tanguy <tanguy@Bulldozer>
2017-12-15 16:44:50 +0100

    Adding CDF plotting to utils

--- plotting/utils.py
+++ plotting/utils.py
@@ -4,6 +4,9 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import multiprocessing
 
+from statsmodels.distributions.empirical_distribution import ECDF
+
+
 
 #This is mainly a wrapper around matplotlib functions, in order to spawn them into processes and avoid multithreading conflicts when plotting on the fly
 
@@ -30,6 +33,39 @@ def plotBarGraph(bars, xlabel = "", ylabel = '', legend = False):
 	return_dict = manager.dict()
 	jobs = []
         p = multiprocessing.Process(target=plotBarGraph_impl, args=(return_dict, bars, xlabel, ylabel, legend))
+        p.start()
+       	p.join()
+
+	return return_dict['graph']
+
+
+
+def plotCDF_impl(return_dict, data, xlabel, ylabel, legend):
+	#plot the image and send it in the response
+	plt.figure()
+
+	for d in data:
+  		cdf = ECDF(d['data'])
+                plt.step(cdf.x, cdf.y, color=d['color'], label = d['label'])
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel)
+	if legend:
+		plt.legend()
+
+	buf = io.BytesIO()
+	plt.savefig(buf, format='svg')
+
+	plt.close()
+	buf.seek(0)
+
+	return_dict['graph'] = buf
+
+
+def plotCDF(data, xlabel = "", ylabel = '', legend = False):
+	manager = multiprocessing.Manager()
+	return_dict = manager.dict()
+	jobs = []
+        p = multiprocessing.Process(target=plotCDF_impl, args=(return_dict, data, xlabel, ylabel, legend))
         p.start()
        	p.join()
 
